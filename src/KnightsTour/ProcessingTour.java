699536fe712eaef754fa8c497a839729b9ff343c
CaptainJensen <hunterjensen9@gmail.com>
2017-01-09 21:03:23 -0500

    FIXED EVERYTHING

--- src/KnightsTour/ProcessingTour.java
+++ src/KnightsTour/ProcessingTour.java
@@ -1,10 +1,8 @@
 package KnightsTour;
 
-import javafx.scene.input.KeyEvent;
 import processing.core.PApplet;
 import processing.core.PFont;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,6 +14,9 @@ public class ProcessingTour extends PApplet {
     private int boardSize = 8;
     private int iterationAwnser = 0;
 
+    Square startingSquare;
+    Knight knight;
+
     private boolean lineMode = false;
 
     private PFont font = createFont("Arial",16,true);
@@ -25,8 +26,9 @@ public class ProcessingTour extends PApplet {
     public void setup() {
         size(boardSize*100,boardSize*100);
 
-        Square startingSquare = new Square((int)random(0,boardSize-1),(int)random(0,boardSize-1),0);//TODO: Make it random
-        Knight knight = new Knight(startingSquare , boardSize, boardSize);
+        startingSquare = new Square((int)random(0,boardSize-1),(int)random(0,boardSize-1),0);
+        knight = new Knight(startingSquare , boardSize, boardSize);
+
         awnsers = knight.solve();
 
         System.out.println(awnsers.toString());
@@ -42,22 +44,28 @@ public class ProcessingTour extends PApplet {
 
         DrawBoard();
 
-        textFont(font,23);
-        fill(0,0,0);
+        textFont(font,40);
 
-
-
-
-
-       // text(String.valueOf(iterationAwnser), centerOfSquareX, centerOfSquareY);
-       // text(String.valueOf(iterationAwnser), height/(boardSize*2), width/(boardSize*2)*14);
 
         for (int i = 0; i < iterationAwnser; i++) {
             if(lineMode) {
-                line(awnsers.get(i).getRow()*(height/boardSize)+ (height/boardSize)/2, awnsers.get(i).getColumn()*(width/boardSize) + (width/boardSize)/2, awnsers.get(i+1).getRow()*(height/boardSize) + (height/boardSize)/2, awnsers.get(i+1).getColumn()*(width/boardSize) + (width/boardSize)/2);
+                if(i<63) {
+                    strokeWeight(2);
+                    line(awnsers.get(i).getRow()*(height/boardSize)+ (height/boardSize)/2, awnsers.get(i).getColumn()*(width/boardSize) + (width/boardSize)/2, awnsers.get(i+1).getRow()*(height/boardSize) + (height/boardSize)/2, awnsers.get(i+1).getColumn()*(width/boardSize) + (width/boardSize)/2);
+
+                }
 
             } else {
-                text( i+1 , awnsers.get(i).getRow()*(height/boardSize)+ (height/boardSize)/2,awnsers.get(i).getColumn()*(width/boardSize) + (width/boardSize)/2);
+                if(i==0) {
+                    fill(0,255,0);
+                }
+                else if(i==63) {
+                    fill(255,0,0);
+                }
+                else {
+                    fill(0,0,0);
+                }
+                text( i+1 , (awnsers.get(i).getRow()*(height/boardSize)+ (height/boardSize)/2)-10,(awnsers.get(i).getColumn()*(width/boardSize) + (width/boardSize)/2)+10);
 
             }
 
@@ -75,10 +83,9 @@ public class ProcessingTour extends PApplet {
 
     public void DrawBoard() {
 
-
-        //TODO:CHANGE
         background (255,255,255);
         stroke(0,0,0);
+        strokeWeight(5);
 
         float h = (float) height/boardSize;
         float v = (float) width/boardSize;
@@ -102,7 +109,7 @@ public class ProcessingTour extends PApplet {
 
         }
         if (keyCode == RIGHT) {
-            if(iterationAwnser < awnsers.size()-1) {
+            if(iterationAwnser < awnsers.size()) {
                 iterationAwnser++;
                 draw();
                 System.out.println("Right Key Pressed & iteration ++");
@@ -110,7 +117,7 @@ public class ProcessingTour extends PApplet {
 
         }
         if (keyCode == UP) {
-            iterationAwnser = awnsers.size()-1;
+            iterationAwnser = awnsers.size();
             draw();
             System.out.println("Up Key Pressed & iteration awnsers.size()-1");
 
@@ -133,6 +140,13 @@ public class ProcessingTour extends PApplet {
             draw();
             System.out.println("Shift Key Pressed & Line mode True");
 
+        }
+        if(keyCode == CONTROL) {
+            startingSquare = new Square((int)random(0,boardSize-1),(int)random(0,boardSize-1),0);
+            knight = new Knight(startingSquare , boardSize, boardSize);
+            iterationAwnser = 0;
+            setup();
+            System.out.println("Controll Key Pressed & Creating new solution");
         }
 
 
